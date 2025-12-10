e294031c10878b23ee307d66cc1a325632a63a90
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-12-10 12:06:31 +0530

    default route /docs

--- MainRoute/mainRoutes.js
+++ MainRoute/mainRoutes.js
@@ -3,15 +3,18 @@ import guestKeyRoutes from "../routes/guestKey.route.js";
 
 /**
  * Registers all routes with base paths
- * @param {Express.Application} app 
- * @param {string} baseUrl 
+ * @param {Express.Application} app
+ * @param {string} baseUrl
  */
 export const registerRoutes = (app, baseUrl = "") => {
-
   // ❗ health route MUST NOT use BASE_URL
-app.get( "/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
-});
+  app.get("/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+  });
+  app.get("/", (req, res) => {
+    res.redirect("/docs");
+  });
+
   app.use(`${baseUrl}`, authRouter);
   app.use(`${baseUrl}`, guestKeyRoutes);
 };
